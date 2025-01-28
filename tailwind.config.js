d009e3f6ef150a604fe829476310eead6ea70719
rikaciminieri <rikaciminieri@gmail.com>
2025-01-28 15:19:28 -0800

    feat: add custom purple for later

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple": "#8134DF",
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
