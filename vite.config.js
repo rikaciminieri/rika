4a3e5d800f9bd833ee8198cb36f4a7c18121a5c0
Rika Ciminieri <rikaciminieri@gmail.com>
2026-03-31 19:04:22 -0700

    feat: clean up csv goodreads code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { letterboxdPlugin } from "./scripts/vite-letterboxd-plugin.js";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), letterboxdPlugin()],
+  plugins: [react()],
 });
